efb46b39e9d35289c64a3f5285a09d6a93bfc28d
tariqkjm7 <d.alatoum@gmail.com>
2021-09-22 22:19:05 +0300

    Rendering Error

--- modules/weather.js
+++ modules/weather.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 const express = require('express');
 
 function weather(req, res) {
-    // res.send('from weather')
+    // res.send('from weather') 
     let q = req.query.city
     console.log(q);
-    // weather key :21d258f0d3504530a7f35033e1d8e43e
+    // weather key :21d258f0d3504530a7f35033e1d8e43e   
     // url : https://api.weatherbit.io/v2.0/forecast/daily?city=Amman&key=21d258f0d3504530a7f35033e1d8e43e
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${q}&key=${process.env.WEATHER_KEY}`
 console.log(url);
@@ -39,3 +39,4 @@ class Weather {
 }
    
 module.exports = weather;
+//   
